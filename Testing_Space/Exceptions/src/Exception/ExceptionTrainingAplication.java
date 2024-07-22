1a7f363b32b31f30458535054b54caaaef0be3bf
Hurokin <marco.vasconcelos@ufpe.br>
2024-07-22 15:33:12 -0300

    Add files via upload

---
+++ Testing_Space/Exceptions/src/Exception/ExceptionTrainingAplication.java
@@ -0,0 +1,29 @@
+package Exception;
+
+import java.util.Scanner;
+
+public class ExceptionTrainingAplication {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        try {
+            int result = divide(a, b);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+}
---
+++ Testing_Space/Exceptions/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Exception_Example_Training {
+}
